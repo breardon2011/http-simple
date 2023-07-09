41900c8660586682c955d6581a5b482b434eb6f7
Brian Reardon <breardon2011@gmail.com>
2023-07-08 18:37:36 -0700

    initial

---
+++ src/http/request.rs
@@ -0,0 +1,60 @@
+use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Formatter,Result as FmtResult,Display, Debug};
+
+pub struct Request { 
+    path: String, 
+    query_string: Option<String>, 
+    method: Method,
+
+}
+
+impl TryFrom<&[u8]> for Request { 
+    type Error = ParseError; 
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest, 
+    InvalidEncoding, 
+    InvalidProtocol,
+    InvalidMethod,
+
+
+}
+
+impl ParseError{
+    fn message(&self) -> &str{
+        match self {
+            Self::InvalidRequest => "InvalidRequest", 
+            Self::InvalidEncoding => "InvalidEncoding", 
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidMethod => "InvalidMethod",
+        
+        }
+
+    }
+}
+
+
+impl Display for ParseError { 
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError { 
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+
+
+impl Error for ParseError { 
+
+}
